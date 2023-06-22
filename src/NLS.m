9bb960be37327f9c5bef73cf6b3fd388521f9415
Nathan Soedjak <n.soedjak@gmail.com>
2023-06-21 22:58:34 -0400

    Fix small typo in reconstruction plots

--- src/NLS.m
+++ src/NLS.m
@@ -113,22 +113,24 @@ disp(' ');
 disp('Plotting final results .......');
 disp(' ');
 
+% Reconsructed coefficients
 k_r = X(1:M);
 gamma_r = X(M+1:2*M);
 sigmaTPA_r = X(2*M+1:3*M);
 sigma_r = X(3*M+1:4*MinVar);
+
 % Plot reconstruction results
 if ismember("k",MinVar)
-    % plot k_t
+    % plot k_r
 end
 if ismember("gamma",MinVar)
-    % plot gamma_t
+    % plot gamma_r
 end
 if ismember("sigmaTPA",MinVar)
-    % plot sigmaTPA_t
+    % plot sigmaTPA_r
 end
 if ismember("sigma",MinVar)
-    % plot sigma_t
+    % plot sigma_r
 end
 
 
