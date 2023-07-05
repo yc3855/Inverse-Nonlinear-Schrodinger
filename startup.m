3456887e5348a752991edec5009b94358f0e4c06
Yan Cheng <yc3855@columbia.edu>
2023-07-05 12:40:39 -0400

    Update startup.m

--- startup.m
+++ startup.m
@@ -1,2 +1,12 @@
-addpath(genpath('Inverse-Nonlinear-Schrodinger/src'));
-addpath(genpath('Inverse-Nonlinear-Schrodinger/test'));
+% Get the full path to the root directory
+root_directory = pwd; % Assuming you are executing startup.m from Inverse-Nonlinear-Schrodinger
+
+% Specify the directories within the root_directory
+src_directory = fullfile(root_directory, 'src');
+test_directory = fullfile(root_directory, 'tests');
+examples_directory = fullfile(root_directory, 'examples');
+
+% Add the directories to the MATLAB search path
+addpath(src_directory);
+addpath(test_directory);
+addpath(examples_directory);
