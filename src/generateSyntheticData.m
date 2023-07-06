2ee4fa7df4fbde64f4f64fecbb25d87288736734
Yan Cheng <yc3855@columbia.edu>
2023-07-06 15:06:36 -0400

    Implemented synthetic data generation

--- src/generateSyntheticData.m
+++ src/generateSyntheticData.m
@@ -1,41 +1,39 @@
-function d = generateSyntheticData(M, Ns, noiselevel, T, F, k_t, gamma_t, sigmaTPA_t, sigma_t)
+function d = generateSyntheticData(Nx, Ny, Nt, dx, dy, dt, sources, ...
+    k_t, sigma_t, sigmaTPA_t, gamma_t, noise)
     % generateSyntheticData Generate noisy data from the solution of the nonlinear
     % Schrödinger equation.
     %
     % Parameters:
-    %   M : int
-    %       The number of measurements.
-    %   Ns : int
-    %       The number of sources.
-    %   noiselevel : float
+    %   Nx, Ny : int
+    %       The number of spatial discretization points along the x and y axes.
+    %   Nt : int
+    %       The number of temporal discretization points.
+    %   dx, dy, dt : float
+    %       The step sizes for spatial and temporal discretization.
+    %   sources : cell array
+    %       The cell array containing the source functions.
+    %   k_t, gamma_t, sigmaTPA_t, sigma_t : float
+    %       The true parameters for the NLS equation.
+    %   noise : float
     %       The level of noise to add to the data.
-    %   T : float
-    %       The final time at which we want the solution.
-    %   F : 2D array (M x Ns)
-    %       The Ns sources/initial conditions for the NLS equation
-    %   k_t : float
-    %       Coefficient for the NLS equation.
-    %   gamma_t : float
-    %       Coefficient for the NLS equation.
-    %   sigmaTPA_t : float
-    %       Coefficient for the NLS equation.
-    %   sigma_t : float
-    %       Coefficient for the NLS equation.
     %
     % Returns:
-    %   d : 2D array (M x Ns)
+    %   d : 4D array (Nx x Ny x Nt x Ns)
     %       The generated data, with added noise.
 
+    % Get the number of sources
+    Ns = length(sources);
+
     % Preallocate the data array
-    d = zeros(M, Ns);
+    d = zeros(Nx, Ny, Nt, Ns);
 
     % Loop over the number of sources
     for s = 1:Ns
-        f_s = F(:,s);
-        % Run NLS equation with initial condition f_s and coefficients
-        d_s = NLS_forward(T, f_s, k_t, gamma_t, sigmaTPA_t, sigma_t);
+        source_s = sources{s};
+        % Run NLS equation with initial condition source_s and coefficients
+        d_s = forward_NLS(Nx, Ny, Nt, dx, dy, dt, source_s, k_t, gamma_t, sigmaTPA_t, sigma_t);
 
         % Add noise to data
-        d(:, s) = d_s * (1 + noiselevel * 2 * (rand(M, 1) - 0.5));
+        d(:, :, :, s) = d_s .* (1 + noise * 2 * (rand(Nx, Ny, Nt) - 0.5));
     end
 end
